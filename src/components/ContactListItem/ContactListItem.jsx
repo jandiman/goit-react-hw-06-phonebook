5677f8b2c9beab2a3eb82e863de105d076b8e378
Jan Dilbert Ampuan <92305240+jandiman@users.noreply.github.com>
2024-03-01 22:42:47 +0800

    added empty redux libary

---
+++ src/components/ContactListItem/ContactListItem.jsx
@@ -0,0 +1,24 @@
+import { FaTrash } from 'react-icons/fa';
+import css from './ContactListItem.module.css';
+import PropTypes from 'prop-types';
+
+export const ContactListItem = ({ filteredContact, deleteContact }) => {
+  const handleDelete = () => {
+    deleteContact(filteredContact.id);
+  };
+
+  return (
+    <li className={css.contactListItem}>
+      <p>{filteredContact.name}:</p>
+      <p>{filteredContact.number}</p>
+      <button onClick={handleDelete}>
+        <FaTrash />
+      </button>
+    </li>
+  );
+};
+
+ContactListItem.propTypes = {
+  filteredContact: PropTypes.object.isRequired,
+  deleteContact: PropTypes.func.isRequired,
+};
